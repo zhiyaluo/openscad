9ffed212b6890122b13177d8a03ba1b9efe7fc03
Don Bright <hmbright@fastmail.com>
2017-09-02 18:23:41 -0500

    temporarily modify shared objects to be executable, prevent ldd warning

--- scripts/nix-setup-gl-libs.sh
+++ scripts/nix-setup-gl-libs.sh
@@ -145,7 +145,10 @@ find_nixstore_dir_for() {
 
 find_shlibs() {
   find_shlib=$1
+  saved_permissions=`stat -c%a $find_shlib`
+  chmod u+x $find_shlib
   shlibs=`ldd $find_shlib | grep "=>" | grep -v "vdso.so" | awk ' { print $3 } ' `
+  chmod $saved_permissions $find_shlib
   echo $shlibs
 }
 
@@ -168,10 +171,10 @@ install_under_specialdir() {
     cp -v $original_path $target_path
   fi
 
-  permissions=`stat -c%a $target_deref_path`
+  saved_permissions=`stat -c%a $target_deref_path`
   chmod u+w $target_deref_path
   patchelf --set-rpath $OSCD_NIXGL_DIR $target_deref_path
-  chmod $permissions $target_deref_path
+  chmod $saved_permissions $target_deref_path
 }
 
 set -e
